cc6648ea997d619b9df8027920eee1df1d5f332f
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-12 19:55:43 +0300

    Create Modal

---
+++ starter/src/components/Modal.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Modal = () => {
+  return (
+    <aside className="modal-container">
+        <div className="modal">
+            <h4>Remove All items from your shopping cart</h4>
+            <div className="btn-container">
+                <button type='button' className="btn confirm-btn">Confirm</button>
+                <button type='button' className="btn clear-btn">Cancel</button>
+            </div>
+        </div>
+    </aside>
+  )
+}
