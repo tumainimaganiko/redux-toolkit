5bdca52a5d2d15c25295a422501a6367f300b5ea
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-12 19:50:01 +0300

    Remove, Increase, Decrease and Calculate

--- starter/src/components/CartItem.jsx
+++ starter/src/components/CartItem.jsx
@@ -1,20 +1,29 @@
 import { ChevronDown, ChevronUp } from "../icons"
+import { useDispatch } from "react-redux";
+import { decrease, increase, removeItem } from "../features/cart/cartSlice";
 
 export const CartItem = ({id, img, title, price, amount}) => {
+    const dispatch = useDispatch();
   return (
     <article className="cart-item">
         <img src={img} alt={title} />
         <div>
             <h4>${title}</h4>
             <h4 className="item-price">${price}</h4>
-            <button className="remove-btn">remove</button>
+            <button className="remove-btn" onClick={ _ => dispatch(removeItem(id))}>remove</button>
         </div>
         <div>
-            <button className="amount-btn">
+            <button className="amount-btn" onClick={ _ => { dispatch(increase({id}))}}>
                 <ChevronUp />
             </button>
             <p className="amount">{amount}</p>
-            <button className="amount-btn">
+            <button className="amount-btn" onClick={ _ => {
+                if(amount === 1){
+                    dispatch(removeItem(id));
+                    return;
+                }
+                dispatch(decrease({id}));
+            }}>
                 <ChevronDown />
             </button>
         </div>
--- starter/src/features/cart/cartSlice.js
+++ starter/src/features/cart/cartSlice.js
@@ -3,7 +3,7 @@ import cartItems from '../../cartItems';
 
 const initialState = {
     cartItems: cartItems,
-    amount: 9,
+    amount: 0,
     total: 0,
     isLoading: true
 }
@@ -13,10 +13,33 @@ const cartSlice = createSlice({
     reducers: {
         clearCart: store => {
             store.cartItems = [];
+        },
+        removeItem: (store,action) => {
+            const itemId = action.payload;
+            store.cartItems = store.cartItems.filter((item) => item.id !== itemId);
+        },
+        increase: (store, { payload }) => {
+            const cartItem = store.cartItems.find((item) => item.id === payload.id);
+            cartItem.amount = cartItem.amount + 1;
+            console.log(payload)
+        },
+        decrease: (store, { payload }) => {
+            const cartItem = store.cartItems.find((item) => item.id === payload.id);
+            cartItem.amount = cartItem.amount - 1;
+        },
+        calculateTotals: (state) => {
+            let amount = 0;
+            let total = 0;
+            state.cartItems.forEach( item => {
+                amount += item.amount;
+                total += item.amount * item.price;
+            })
+            state.amount = amount;
+            state.total = total;
         }
     }
 })
 // console.log(cartSlice);
-export const { clearCart } = cartSlice.actions;
+export const { clearCart, removeItem, increase, decrease, calculateTotals } = cartSlice.actions;
 
 export default cartSlice.reducer;
