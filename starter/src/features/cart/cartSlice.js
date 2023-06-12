67580410639b02bb24177113e3bc7ce70303aee9
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-12 21:42:54 +0300

    Async Functionality

--- starter/src/features/cart/cartSlice.js
+++ starter/src/features/cart/cartSlice.js
@@ -1,12 +1,17 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import cartItems from '../../cartItems';
 
+const url = 'https://course-api.com/react-useReducer-cart-project';
 const initialState = {
-    cartItems: cartItems,
+    cartItems: [],
     amount: 0,
     total: 0,
     isLoading: true
 }
+
+export const getCartItems = createAsyncThunk('cart/getCartItems', _ => {
+    return fetch(url).then(resp => resp.json()).catch(err => console.log(err))
+})
 const cartSlice = createSlice({
     name: 'cart',
     initialState,
@@ -37,6 +42,19 @@ const cartSlice = createSlice({
             state.amount = amount;
             state.total = total;
         }
+    },
+    extraReducers : {
+        [getCartItems.pending] : state => {
+            state.isLoading = true;
+        },
+        [getCartItems.fulfilled] : (state, action) => {
+            console.log(action);
+            state.isLoading = false;
+            state.cartItems = action.payload;
+        },
+        [getCartItems.rejected] : state => {
+            state.isLoading = false;
+        },
     }
 })
 // console.log(cartSlice);
