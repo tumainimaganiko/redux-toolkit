cfc1268dbda747ce8621c1bc34864b381b474930
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-12 20:30:15 +0300

    Toggle Modal

--- starter/src/components/CartContainer.jsx
+++ starter/src/components/CartContainer.jsx
@@ -1,6 +1,6 @@
 import { CartItem } from "./CartItem";
 import { useSelector, useDispatch } from "react-redux";
-import { clearCart } from "../features/cart/cartSlice";
+import { openModal } from "../features/modal/modalSlice";
 
 export const CartContainer = () => {
     const dispatch = useDispatch();
@@ -30,7 +30,7 @@ export const CartContainer = () => {
             <div className="cart-total">
                 <h4>total <span>${total.toFixed(2)}</span></h4>
             </div>
-            <button className="btn clear-btn" onClick={() => {dispatch(clearCart())}}>clear cart</button>
+            <button className="btn clear-btn" onClick={() => {dispatch(openModal())}}>clear cart</button>
         </footer>
     </section>
   )
--- starter/src/components/Modal.jsx
+++ starter/src/components/Modal.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import { useDispatch } from "react-redux";
+import { closeModal } from "../features/modal/modalSlice";
+import { clearCart } from "../features/cart/cartSlice";
 
 export const Modal = () => {
+    const dispatch = useDispatch()
   return (
     <aside className="modal-container">
         <div className="modal">
             <h4>Remove All items from your shopping cart</h4>
             <div className="btn-container">
-                <button type='button' className="btn confirm-btn">Confirm</button>
-                <button type='button' className="btn clear-btn">Cancel</button>
+                <button type='button' className="btn confirm-btn" onClick={
+                    _ => {
+                        dispatch(clearCart());
+                        dispatch(closeModal());
+                    }
+                }>Confirm</button>
+                <button type='button' className="btn clear-btn" onClick={
+                    _ => dispatch(closeModal())
+                }>Cancel</button>
             </div>
         </div>
     </aside>
