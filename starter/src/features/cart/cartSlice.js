522adea284dc489b54534eb98403b5f66975e210
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-12 17:58:48 +0300

    Setup Cart

--- starter/src/features/cart/cartSlice.js
+++ starter/src/features/cart/cartSlice.js
@@ -1,8 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
+import cartItems from '../../cartItems';
 
 const initialState = {
-    cartItems: [],
-    amount: 8,
+    cartItems: cartItems,
+    amount: 9,
     total: 0,
     isLoading: true
 }
